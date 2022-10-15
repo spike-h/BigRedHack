1a3aae19201ce24ad45529f1312aace2ec1141ea
siyn <ec769@cornell.edu>
2022-10-15 13:59:07 -0400

    functional website now

--- website/mysite/urls.py
+++ website/mysite/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
     path('dud/', include('dud.urls')),
+    path('sustainability/', include('sustainability.urls')),
     path('admin/', admin.site.urls),
 ]
